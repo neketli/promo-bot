8480a5413598d7fc1433136356de7a7d4500c32a
n.li <n.li@ddos-guard.net>
2023-01-24 18:34:30 +0300

    Removed server and math operations from old calc-bot

--- internal/promo-bot/calc-bot.go
+++ internal/promo-bot/calc-bot.go
@@ -7,7 +7,6 @@ import (
 	tgClient "promo-bot/internal/promo-bot/clients/telegram"
 	event_consumer "promo-bot/internal/promo-bot/consumer/event-consumer"
 	"promo-bot/internal/promo-bot/events/telegram"
-	"promo-bot/internal/promo-bot/server"
 	"promo-bot/internal/promo-bot/storage/sqlite"
 )
 
@@ -29,12 +28,6 @@ func Start(config *config.Config) {
 	eventsHandler := telegram.New(tgClient.New(config.TG.TgHost, config.TG.TgToken), storage)
 
 	log.Printf("Service has been started")
-
-	go func() {
-		if err := server.Start(config, storage); err != nil {
-			log.Fatal("Service's server stopped: ", err)
-		}
-	}()
 
 	consumer := event_consumer.New(eventsHandler, eventsHandler, batchSize)
 	if err := consumer.Start(); err != nil {
